75eecb7ac12770f49e08d44f062183480956f57b
Mohammad <mohammadbabaeesem@gmail.com>
2023-02-28 10:24:43 +0330

    update code

--- app/app.py
+++ app/app.py
@@ -1,22 +1,42 @@
-# file app.py
-
+# -*- coding: utf-8 -*-
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.graph_objects as go
+from dash import html
+from plotly.data import iris
 
-es = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=es)
+import holoviews as hv
+from holoviews import opts
+from holoviews.plotting.plotly.dash import to_dash
 
-xs = list(range(30))
-ys = [10000 * 1.07**i for i in xs]
+# Load dataset
+df = iris()
+dataset = hv.Dataset(df)
 
-fig = go.Figure(data=go.Scatter(x=xs, y=ys))
-fig.update_layout(xaxis_title='Years', yaxis_title='$')
+# Build selection linking object
+selection_linker = hv.selection.link_selections.instance()
+scatter = selection_linker(
+    hv.Scatter(dataset, kdims=["sepal_length"], vdims=["sepal_width"])
+)
+hist = selection_linker(
+    hv.operation.histogram(dataset, dimension="petal_width", normed=False)
+)
 
-app.layout = html.Div(children=[
-    html.H1(children='Assets'),
-    dcc.Graph(figure=fig)])
+# Use plot hook to set the default drag mode to box selection
+def set_dragmode(plot, element):
+    fig = plot.state
+    fig['layout']['dragmode'] = "select"
+    if isinstance(element, hv.Histogram):
+        # Constrain histogram selection direction to horizontal
+        fig['layout']['selectdirection'] = "h"
 
-if __name__ == '__main__':
+scatter.opts(opts.Scatter(hooks=[set_dragmode]))
+hist.opts(opts.Histogram(hooks=[set_dragmode]))
+
+app = dash.Dash(__name__)
+components = to_dash(
+    app, [scatter, hist], reset_button=True
+)
+
+app.layout = html.Div(components.children)
+
+if __name__ == "__main__":
     app.run_server(debug=True)
